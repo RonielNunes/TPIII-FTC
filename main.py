6d9e62d7171a65ba0311222eab50a2fda4599e81
Arthur Sales da Silva <72162624+ArthurSales99@users.noreply.github.com>
2021-10-19 00:41:48 -0300

    correção na verificação dos casos de teste

--- main.py
+++ main.py
@@ -19,15 +19,16 @@ class Alfabeto:
 
 
 input_file      = open("c:/Users/arthu/Desktop/TP FTC/TPIII-FTC/init.txt", "r")
-estados_input   = input_file.readline().replace("Q: ", "").replace("\n", "").split(" ") # input("Q: ").split(" ")
-estado_inicial  = input_file.readline().replace("I: ", "").replace("\n", "") #input("I: ")
+estados_input   = input_file.readline().replace("Q: ", "").replace("\n", "").split(" ")
+estado_inicial  = input_file.readline().replace("I: ", "").replace("\n", "")
 
 if (estado_inicial not in estados_input):
     print("\nEstado inicial inválido!")
 else:
-    estados_finais  = input_file.readline().replace("F: ", "").replace("\n", "") # input("F: ")
+    estados_finais  = input_file.readline().replace("F: ", "").replace("\n", "")
     estados         = []
 
+    # Percorre os estados do input criando um objeto Estado para cada e adicionando na lista estados
     for estado in estados_input:
 
         flag_ini = False
@@ -46,21 +47,24 @@ else:
     transicoes      = []
     transicao_input = ""
 
+    # Percorre as transições do input criando um objeto Transicao para cada e adicionando na lista de transições do estado origem da transição
     for linha in input_file:
         transicao_input = linha.replace("\n", "").split(" -> ") # input().split(" -> ")
 
         if (transicao_input[0] == "---"):
             break
 
-        estado_um = transicao_input[0]
-        estado_dois = transicao_input[1].split(" | ")[0]
-        valores = transicao_input[1].split(" | ")[1].split(" ")
+        estado_origem   = transicao_input[0]
+        estado_destino  = transicao_input[1].split(" | ")[0]
+        valores         = transicao_input[1].split(" | ")[1].split(" ")
 
-        transicao = Transicao(valores, estado_dois)
+        transicao       = Transicao(valores, estado_destino)
 
+        # Percorre os estados para encontrar o estado de origem da transição e adicionar a transição à lista de transições deste estado
         for estado in estados:
-            if (estado == estado_um):
+            if (estado.nome == estado_origem):
                 estado.transicoes.append(transicao)
+                break
 
     casos_teste = []
 
@@ -68,35 +72,43 @@ else:
         entry = linha.replace("\n", "")
         casos_teste.append(entry)
 
-
     for caso in casos_teste:
-
-        estado_atual = estado_inicial
-        flag_erro = False
+        # Reseta o estado atual para o inicial para cada novo caso de teste
+        estado_atual    = estado_inicial
+        flag_erro       = False
 
         # Percorre os valores do caso atual
         for valor in range(len(caso)):
+            
             # Percorre a lista de estados
             for estado in estados:
+
                 # Seleciona o estado do estado atual
                 if (estado_atual == estado.nome):
+
                     # Percorre as transições do estado atual
                     for trans in range(len(estado.transicoes)):
+
                         # Se encontra uma transição com o mesmo valor atual troca de estado atual
-                        if (estado.transicoes[trans].valor == caso[valor]):
+                        if (caso[valor] in estado.transicoes[trans].valores):
                             estado_atual = estado.transicoes[trans].estado_dest
+                            break
 
                         # Se no final das transições não encontrar nenhuma transição com o mesmo valor, o caso deu errado
                         if(trans == len(estado.transicoes) - 1):
-                            #print("X")
                             flag_erro = True
 
-                    # Se no final do caso o estado atual for um estado final, o caso de certo, caso contrário deu errado
-                    if (valor == len(caso) - 1):
-                        if (estado.flag_final):
-                            flag_erro = False
-                        else:
-                            flag_erro = True
+                    break # break para sair do for de estados e ir para o próximo valor (estava causando problema pois o estado_atual é alterado dentro do for)
+
+        # Se no final do caso o estado atual for um estado final, o caso deu certo, caso contrário deu errado
+        for estado in estados:
+            if (estado_atual == estado.nome):
+                if (estado.flag_final):
+                    flag_erro = False
+                else:
+                    flag_erro = True
+                
+                break
 
         if(flag_erro):
             print("X")
