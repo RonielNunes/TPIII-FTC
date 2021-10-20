3381f09c2c8ba201c391f8003563afae9d28eace
ViniciusTei <viniciustprates@gmail.com>
2021-10-19 23:45:04 -0300

    add afn
    Co-authored-by: buMatheus <buMatheus@users.noreply.github.com>
    Co-authored-by: Lucas Takeshi <LTaka@users.noreply.github.com>
    Co-authored-by: Roniel Nunes <RonielNunes@users.noreply.github.com>

--- automaton/automaton.py
+++ automaton/automaton.py
@@ -1,4 +1,14 @@
-
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    RESET = '\x1b[0m'
 
 class Transicao:
     def __init__(self, valores, estado_dest):
@@ -59,9 +69,9 @@ class AFD:
                     break
 
             if(flag_erro):
-                print("X")
+                print(bcolors.FAIL + "X" + bcolors.RESET)
             else:
-                print("OK")
+                print(bcolors.OKGREEN + "OK" + bcolors.RESET)
 
 
 class Alfabeto:
@@ -71,9 +81,6 @@ class Alfabeto:
     def checkIsInAlphabet(self, entry) -> bool:
         isValid = True
 
-        #verifica se eh palavra vazia
-        if(len(entry) == 0):
-            return False
         #para cada caractere na string de teste checamos se ele esta no alfabeto
         for char_entry in entry:
             isIn = False
@@ -103,7 +110,7 @@ class AFD_ALPHABET(AFD):
             isValid = self.alfabeto.checkIsInAlphabet(caso)
 
             if(isValid == False):
-                print("X")
+                print(bcolors.FAIL + "X" + bcolors.RESET)
                 continue
 
             # Percorre os valores do caso atual
@@ -140,8 +147,76 @@ class AFD_ALPHABET(AFD):
                     break
 
             if(flag_erro):
-                print("X")
+                print(bcolors.FAIL + "X" + bcolors.RESET)
             else:
-                print("OK")
+                print(bcolors.OKGREEN + "OK" + bcolors.RESET)
 
+
+class AFN(AFD_ALPHABET):
+    def __init__(self, estado_inicial, estados, casos_teste, alfabeto):
+        super().__init__(estado_inicial, estados, casos_teste, alfabeto)
     
+    def verificaCasosDeTeste(self):
+        
+        for caso in self.casos_teste:
+            #para cada estado inicial fazemos um teste, se todos falharem nao temos um teste valido
+            cont_erro = 0
+            
+            for estado_in in self.estado_inicial:
+                # Reseta o estado atual para o inicial para cada novo caso de teste
+                estado_atual    = estado_in
+                #flag_erro       = False
+
+                #verifica se o caso bate com o alfabeto se for false encerra o casoe vai para o proximo
+                isValid = self.alfabeto.checkIsInAlphabet(caso)
+
+                if(isValid == False):
+                    #print(bcolors.WARNING + "is not valid" + bcolors.RESET)
+                    cont_erro += 1
+                    continue
+
+                # Percorre os valores do caso atual
+                for valor in range(len(caso)):
+                    
+                    # Percorre a lista de estados
+                    for estado in self.estados:
+
+                        # Seleciona o estado do estado atual
+                        if (estado_atual == estado.nome):
+                            flag_estado_atual_para_ele_mesmo = False
+                            # Percorre as transições do estado atual
+                            for transicao_index in range(len(estado.transicoes)):
+
+                                # Se encontra uma transição com o caracter do estado atual, troca de estado atual
+                                if (caso[valor] in estado.transicoes[transicao_index].valores):
+                                    if (estado_atual == estado.transicoes[transicao_index].estado_dest):
+                                        flag_estado_atual_para_ele_mesmo = True
+                                        continue
+                                    else:
+                                        estado_atual = estado.transicoes[transicao_index].estado_dest
+                                        break
+
+                                # Se no final das transições não encontrar nenhuma transição com o mesmo valor, o caso deu errado
+                                if(transicao_index == len(estado.transicoes) - 1 and flag_estado_atual_para_ele_mesmo == False):
+                                    #flag_erro = True
+                                    cont_erro += 1
+
+                            break # break para sair do for de estados e ir para o próximo valor (estava causando problema pois o estado_atual é alterado dentro do for)
+
+                # Se no final do caso o estado atual for um estado final, o caso deu certo, caso contrário deu errado
+                for estado in self.estados:
+                    if (estado_atual == estado.nome):
+                        if (estado.flag_final == False):
+                            cont_erro += 1
+                        #     flag_erro = False
+                        # else:
+                        #     #flag_erro = True
+                        
+                        break
+
+            # se o cont_erro for do mesmo valor da quantidade de estados iniciais, nenhuma verificacao validou
+            #print(cont_erro, len(self.estado_inicial))
+            if(cont_erro > 0):
+                print(bcolors.FAIL + "X" + bcolors.RESET)
+            else:
+                print(bcolors.OKGREEN + "OK" + bcolors.RESET)
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from pathlib import Path
-from automaton.automaton import AFD_ALPHABET, Transicao, Estado, AFD
+from automaton.automaton import AFD_ALPHABET, Transicao, Estado, AFD, bcolors, AFN
 
 def testAFD(file_name):
+    print(bcolors.UNDERLINE + bcolors.OKCYAN + "\nAFD\n" + bcolors.RESET)
     input_file      = open(Path(file_name), "r")
     estados_input   = input_file.readline().replace("Q: ", "").replace("\n", "").split(" ")
     estado_inicial  = input_file.readline().replace("I: ", "").replace("\n", "")
@@ -10,7 +11,7 @@ def testAFD(file_name):
     # alfabeto.checkIsInAlphabet('a7b88bba')
 
     if (estado_inicial not in estados_input):
-        print("\nEstado inicial inválido!")
+        print(bcolors.FAIL + "\nEstado inicial inválido!" + bcolors.RESET)
     else:
         estados_finais  = input_file.readline().replace("F: ", "").replace("\n", "")
         estados         = []
@@ -65,13 +66,14 @@ def testAFD(file_name):
     afd.verificaCasosDeTeste()
 
 def test_afd_multiple_alphabet(file_name):
+    print(bcolors.UNDERLINE + bcolors.OKCYAN + "\nAFD alfabeto\n" + bcolors.RESET)
     input_file      = open(Path(file_name), "r")
     estados_input   = input_file.readline().replace("Q: ", "").replace("\n", "").split(" ")
     alfabeto = input_file.readline().replace("S: ", "").replace("\n", "")
     estado_inicial  = input_file.readline().replace("I: ", "").replace("\n", "")
 
     if (estado_inicial not in estados_input):
-        print("\nEstado inicial inválido!")
+        print(bcolors.FAIL + "\nEstado inicial inválido!" + bcolors.RESET)
     else:
         estados_finais  = input_file.readline().replace("F: ", "").replace("\n", "")
         estados         = []
@@ -124,10 +126,81 @@ def test_afd_multiple_alphabet(file_name):
     #inicia um afd 
     afdf = AFD_ALPHABET(estado_inicial=estado_inicial, estados=estados, casos_teste=casos_teste, alfabeto=alfabeto)
     afdf.verificaCasosDeTeste()
+#Definição de Teste de AFN
+def test_afn(file_name):
+    print(bcolors.UNDERLINE + bcolors.OKCYAN + "\nAFN\n" + bcolors.RESET)
+
+    input_file      = open(Path(file_name), "r")
+    estados_input   = input_file.readline().replace("Q: ", "").replace("\n", "").split(" ")
+    alfabeto = input_file.readline().replace("S: ", "").replace("\n", "")
+    estado_inicial  = input_file.readline().replace("I: ", "").replace("\n", "").split(" ")
+
+    if (all(item in estados_input for item in estado_inicial) == False):
+        print(bcolors.FAIL + "\nEstado inicial inválido!" + bcolors.RESET)
+    else:
+        estados_finais  = input_file.readline().replace("F: ", "").replace("\n", "")
+        estados         = []
+        
+        # Percorre os estados do input criando um objeto Estado para cada e adicionando na lista estados
+        for estado in estados_input:
+
+            flag_ini = False
+            flag_fin = False
+
+            if (estado in estado_inicial):
+                flag_ini = True
+
+            if (estado in estados_finais):
+                flag_fin = True
+
+            est = Estado(estado, flag_ini, flag_fin, [])
+
+            estados.append(est)
+
+        transicoes      = []
+        transicao_input = ""
+
+        # Percorre as transições do input criando um objeto Transicao para cada e adicionando na lista de transições do estado origem da transição
+        for linha in input_file:
+            transicao_input = linha.replace("\n", "").split(" -> ") # input().split(" -> ")
+
+            if (transicao_input[0] == "---"):
+                break
+
+            estado_origem   = transicao_input[0]
+            estado_destino  = transicao_input[1].split(" | ")[0]
+            valores         = transicao_input[1].split(" | ")[1].split(" ")
+
+            transicao       = Transicao(valores, estado_destino)
+
+            # Percorre os estados para encontrar o estado de origem da transição e adicionar a transição à lista de transições deste estado
+            for estado in estados:
+                if (estado.nome == estado_origem):
+                    estado.transicoes.append(transicao)
+                    break
+
+        
+        casos_teste = []
+
+        for linha in input_file:
+            entry = linha.replace("\n", "")
+            casos_teste.append(entry)
+
+    # Inicia AFN
+    #Verificação de labda
+    if('\\' in alfabeto):
+        #Inválido caso haja lambda no alfabeto
+        print(bcolors.FAIL + "Alfabeto invalido!\n" + bcolors.RESET)
+    else:
+        #Alfabeto Válido
+        afn = AFN(estado_inicial=estado_inicial, estados=estados, casos_teste=casos_teste, alfabeto=alfabeto)
+        afn.verificaCasosDeTeste()
 
 def __main__():
-    print("Testes")
+    testAFD('init.txt')
 
     test_afd_multiple_alphabet('alfabeto.txt')
+
+    test_afn('afn2.txt')
 
 __main__()
